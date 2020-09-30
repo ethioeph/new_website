d9cccac01d7679d0591aabd7e3021fa0fb4d88ee
Daniel Woldegiorgis <dww2@williams.edu>
2020-09-30 03:50:06 -0600

    production build

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,7 +2,6 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
-  "component---cache-dev-404-page-js": () => import("/Users/danielwoldegiorgis/Downloads/personal-website/new_website/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---src-pages-404-js": () => import("/Users/danielwoldegiorgis/Downloads/personal-website/new_website/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
   "component---src-pages-activities-js": () => import("/Users/danielwoldegiorgis/Downloads/personal-website/new_website/src/pages/activities.js" /* webpackChunkName: "component---src-pages-activities-js" */),
   "component---src-pages-contact-js": () => import("/Users/danielwoldegiorgis/Downloads/personal-website/new_website/src/pages/contact.js" /* webpackChunkName: "component---src-pages-contact-js" */),
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/danielwoldegiorgis/Downloads/personal-website/new_website/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/danielwoldegiorgis/Downloads/personal-website/new_website/src/pages/404.js"))),
   "component---src-pages-activities-js": hot(preferDefault(require("/Users/danielwoldegiorgis/Downloads/personal-website/new_website/src/pages/activities.js"))),
   "component---src-pages-contact-js": hot(preferDefault(require("/Users/danielwoldegiorgis/Downloads/personal-website/new_website/src/pages/contact.js"))),
